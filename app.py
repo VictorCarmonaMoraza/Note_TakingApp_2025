8adfe7525bbf3189dd7b2b7b435cd506fa0c0a83
Victor Carmona <victorcarmona9035.com>
2025-10-01 18:58:39 +0200

    Leer,escribir y añadir contenido a ficheros

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+
+##Abrir fichero y lo cierra automaticamente
 with open("files/fichero1.txt","r",encoding="utf-8") as file:
+    content = file.read()
+    print(content)
+
+##Escribir en un archivo
+#Si el fichero no existe lo crea
+with open("files/fichero2.txt","w",encoding="utf-8") as file:
+    file.write("\nEste es el nuevo texto creado.\n")
+
+##Volvemos a leer el fichero
+with open("files/fichero1.txt","r",encoding="utf-8") as file:
+    content = file.read()
+    print(content)
+
+with open("files/fichero1.txt", "a", encoding="utf-8") as file:
+    file.write("\nLo añade al final del fichero.\n")
+
+## Leer de nuevo para mostrar los cambios
+with open("files/fichero1.txt", "r", encoding="utf-8") as file:
     content = file.read()
     print(content)
